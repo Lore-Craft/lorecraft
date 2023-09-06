519c722e5fc180bd3d8b1c6ac9b25ca5f3f18b0f
Quasark <q@quasark.xyz>
2023-09-06 15:18:09 -0700

    weird problems gone, restructuring the schema

--- server.js
+++ server.js
@@ -5,21 +5,20 @@ const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 const mongoose = require('mongoose');
 const User = require('./UserModel');
 
 app.use(cors());
 app.use(express.json());
+// app.use(authorize);
 
-// nosql stuff
-const MongoURL = 'mongodb://localhost:27017';
-const dbName = 'minecraft_db';
-
+mongoose.connect(MONGODB_URL);
 
 // Create a new Character
 app.post('/character', async (req, res) => {
-  console.log('creating new character: ', req.body)
+  // console.log('creating new character: ', req.body)
   try {
     const newCharacter = new User(req.body);
     const savedCharacter = await newCharacter.save();
@@ -32,11 +31,11 @@ app.post('/character', async (req, res) => {
 
 // Read all Characters
 app.get('/character', async (req, res) => {
-  console.log('getting list of characters.')
+  // console.log('getting list of characters.')
   try {
-    console.log('querying')
+    // console.log('querying')
     const characters = await User.find({});
-    console.log('result: ', characters)
+    // console.log('result: ', characters)
     res.json(characters);
   } catch (e) {
     res.status(500).send(e);
@@ -47,21 +46,27 @@ app.get('/character', async (req, res) => {
 app.patch('/character/:userEmail', async (req, res) => {
   const { userEmail } = req.params;
   // console.log('userEmail: ' + userEmail);
+  console.log('userEmail', userEmail);
   try {
-  console.log('trying...');
-    // Use the $push operator to append new characters to the existing array.
-    const updatedUser = await User.findOneAndUpdate(
-      { userEmail },
-      { $push: { characters: { $each: req.body.characters } } },
-      { new: true }
-    );
-    console.log('updatedUser: ', updatedUser)
-    if (updatedUser) {
-      res.status(200).json(updatedUser);
-    } else {
-      res.status(404).send("User with that email address not found");
+    const incomingCharacters = req.body.characters; // Assuming the incoming JSON has a "characters" property
+
+    // Find the user by userEmail
+    const user = await User.findOne({ userEmail });
+
+    if (!user) {
+      return res.status(404).send("User with that email address not found");
     }
+
+    // Append the incoming characters to the user's characters array
+    user.characters.push(...incomingCharacters);
+
+    // Save the updated user document
+    const updatedUser = await user.save();
+
+    console.log('updatedUser: ', updatedUser);
+    res.status(200).json(updatedUser);
   } catch (e) {
+    console.error('Error:', e);
     res.status(500).send(e);
   }
 });
