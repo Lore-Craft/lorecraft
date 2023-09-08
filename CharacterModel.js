68f412a3af332dc668d756f7a6b09ef1a5aac523
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-09-07 19:57:25 -0700

    updated character schema

--- CharacterModel.js
+++ CharacterModel.js
@@ -7,6 +7,7 @@ const CharacterSchema = new Schema({
   charName: { type: String, required: true },
   classType: { type: String, required: true },
   alignment: { type: String, required: true },
+  race: {type: String, required: true},
   gender: { type: String, required: true },
   imageURL: { type: String, required: true },
   backstory: { type: String, required: true },
